efb477caf4173f3e070d81dbe7a2f1c9ae2eb08e
Carlos Briones <CBRIONE4@externos.bancoestado.cl>
2022-11-07 20:59:02 -0300

    first commit mongoose

---
+++ app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var port = 8083;
+//var db = 'mongodb+srv://cbdev:clavemongodb@cluster0.ny18irn.mongodb.net/?retryWrites=true&w=majority';
+//var db = 'mongodb://cbdev:clavemongodb@cluster0.ny18irn.mongodb.net';
+//mongoose.connect(db);
+
+// 
+const mongoAtlasUri =
+  "mongodb+srv://cbdev:clavemongodb@cluster0.ny18irn.mongodb.net/books?retryWrites=true&w=majority";
+
+try {
+  // Connect to the MongoDB cluster
+  mongoose.connect(
+    mongoAtlasUri,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log(" Mongoose is connected"),
+  );
+} catch (e) {
+  console.log("could not connect");
+}
+
+
+//
+
+var books = require('./routes/books');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use('/books', books);
+
+app.get('/', function(req, res){
+    console.log('app starting on port: '+port)
+    res.send('tes express nodejs mongodb');
+});
+
+app.listen(port, function(){
+    console.log('app listening on port: '+port);
+});
