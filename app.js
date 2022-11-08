74a029a9fc1cc1fe673233d2189e733c4f663a6f
Carlos Briones <CBRIONE4@externos.bancoestado.cl>
2022-11-07 22:18:04 -0300

    port

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ app.get('/', function(req, res){
     res.send('tes express nodejs mongodb');
 });
 
+const port = process.env.PORT || 3000;
+
 app.listen(port, function(){
     console.log('app listening on port: '+port);
 });
