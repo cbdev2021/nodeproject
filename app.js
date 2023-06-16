5612d3e7d891410dea0e2cffe42819425332061b
carlos <cbdev2021@gmail.com>
2023-06-16 02:32:44 -0400

    5000

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 //var port = 8083;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 //const PORT = 3000;
 
 //var db = 'mongodb+srv://cbdev:clavemongodb@cluster0.ny18irn.mongodb.net/?retryWrites=true&w=majority';
